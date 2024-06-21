b69c4eedf644591eac2931c511b4596b9c79e18d
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-06-21 13:43:24 -0500

    Updated gitignore

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();  // Load environment variables from .env file
-
-export const environment = {
-  production: true,
-  contactKey: process.env['formskey'],
-};
